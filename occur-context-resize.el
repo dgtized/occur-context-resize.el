317be306eb359759c792b7cb98f39b81b269a741
Charles Comstock <dgtized@gmail.com>
2014-08-24 17:15:10 -0500

    Fix require for emacs 24

--- occur-context-resize.el
+++ occur-context-resize.el
@@ -7,7 +7,7 @@
 ;; Version: 0.1
 ;; URL: https://github.com/dgtized/occur-context-resize.el
 ;; Keywords: matching
-;; Package-Requires: ((emacs "24))
+;; Package-Requires: ((emacs "24"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
